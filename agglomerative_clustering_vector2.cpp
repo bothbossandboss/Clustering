6c4a088e0ac15402bc23645e98318499aba0da67
bothbossandboss <bothboss21@gmail.com>
2015-06-20 19:06:47 +0900

    多次元用の凝集型クラスタリングプログラムを作成

---
+++ agglomerative_clustering_vector2.cpp
@@ -0,0 +1,21 @@
+/**
+ * 多次元データを凝集型クラスタリングで分類する。
+ * まずは2次元データを扱う。
+ * 多次元ガウス分布(共分散=0)から出力された正解のデータをクラスタリングすることを考える。
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <iostream>
+#include <vector>
+
+#define SINGLE 1
+#define COMPLETE 2
+#define CENTROID 3
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	return 0;
+}
