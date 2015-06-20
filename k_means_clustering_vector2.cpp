89ccebc8eeab013e471ab58eaa19e0e2be371432
bothbossandboss <bothboss21@gmail.com>
2015-06-20 23:13:17 +0900

    k-meansの多次元版を作成

---
+++ k_means_clustering_vector2.cpp
@@ -0,0 +1,21 @@
+/**
+ * 多次元データをk-meansクラスタリングで分類する。
+ * まずは2次元データを扱う。
+ * 多次元ガウス分布(共分散=0)から出力された正解のデータをクラスタリングすることを考える。
+ * k = 2 (2つのクラスタに分類)とする。
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define K 2
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	return 0;
+}
